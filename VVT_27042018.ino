54810e105aed54ba457252139f2558d60fef7e90
Dancake <danthecake@googlemail.com>
2018-04-27 20:53:40 +0100

    Add files via upload

---
+++ VVT_27042018.ino
@@ -0,0 +1,64 @@
+int camAngles[]={20,360,400};
+int crankAngles[]={100,140,250,320,430,500,610,680};
+volatile unsigned long crankTimes[9];
+volatile unsigned long camTimes[4];
+volatile int toothCurrentCount;
+volatile int secondaryToothCount;
+volatile int secondaryToothCounter;
+bool readyForCalc = false;
+void setup() {
+ 
+
+}
+
+void loop() {
+
+
+if (readyForCalc){
+  
+noInterrupts();  
+unsigned long tempCrankTimes [9];
+unsigned long tempCamTimes[4];
+tempCamTimes[1] = camTimes[1];
+tempCamTimes[2] = camTimes[2];
+tempCamTimes[3] = camTimes[3];
+
+tempCrankTimes[1] = crankTimes[1];
+tempCrankTimes[2] = crankTimes[2];
+tempCrankTimes[3] = crankTimes[3];
+tempCrankTimes[4] = crankTimes[4];
+tempCrankTimes[5] = crankTimes[5];
+tempCrankTimes[6] = crankTimes[6];
+tempCrankTimes[7] = crankTimes[7];
+tempCrankTimes[8] = crankTimes[8];
+interrupts();
+
+}
+
+}
+
+void crankTrigger()
+{
+  //add some filtering
+  toothCurrentCount++;
+  if ((secondaryToothCount == 2) || (toothCurrentCount >= 9))
+  {
+    toothCurrentCount  = 1;
+  }
+  crankTimes[toothCurrentCount] = micros();
+  secondaryToothCount = 0;
+
+}
+
+void camTrigger()
+{
+  //write some filtering
+  secondaryToothCount++;
+  secondaryToothCounter++;
+  if ((secondaryToothCount == 2) || (secondaryToothCounter >= 4))
+{
+  secondaryToothCounter = 1;
+}
+camTimes[secondaryToothCounter] = micros();
+}
+
---
+++ timers.ino
@@ -0,0 +1,174 @@
+/*
+Speeduino - Simple engine management for the Arduino Mega 2560 platform
+Copyright (C) Josh Stewart
+A full copy of the license may be found in the projects root directory
+*/
+
+/*
+Timers are used for having actions performed repeatedly at a fixed interval (Eg every 100ms)
+They should not be confused with Schedulers, which are for performing an action once at a given point of time in the future
+
+Timers are typically low resolution (Compared to Schedulers), with maximum frequency currently being approximately every 10ms
+*/
+#include "timers.h"
+
+
+#if defined(CORE_AVR)
+  #include <avr/wdt.h>
+#endif
+
+void initialiseTimers()
+{
+#if defined(CORE_AVR) //AVR chips use the ISR for this
+   //Configure Timer2 for our low-freq interrupt code.
+   TCCR2B = 0x00;          //Disbale Timer2 while we set it up
+   TCNT2  = 131;           //Preload timer2 with 131 cycles, leaving 125 till overflow. As the timer runs at 125Khz, this causes overflow to occur at 1Khz = 1ms
+   TIFR2  = 0x00;          //Timer2 INT Flag Reg: Clear Timer Overflow Flag
+   TIMSK2 = 0x01;          //Timer2 Set Overflow Interrupt enabled.
+   TCCR2A = 0x00;          //Timer2 Control Reg A: Wave Gen Mode normal
+   /* Now configure the prescaler to CPU clock divided by 128 = 125Khz */
+   TCCR2B |= (1<<CS22)  | (1<<CS20); // Set bits
+   TCCR2B &= ~(1<<CS21);             // Clear bit
+
+   //Enable the watchdog timer for 2 second resets (Good reference: https://tushev.org/articles/arduino/5/arduino-and-watchdog-timer)
+   //Boooooooooo WDT is currently broken on Mega 2560 bootloaders :(
+   //wdt_enable(WDTO_2S);
+
+#elif defined (CORE_TEENSY)
+   //Uses the PIT timer on Teensy.
+   lowResTimer.begin(oneMSInterval, 1000);
+
+#elif defined(CORE_STM32)
+#if defined(ARDUINO_BLACK_F407VE) || defined(STM32F4) || defined(_STM32F4_)
+  Timer8.setPeriod(1000);  // Set up period
+  Timer8.setMode(1, TIMER_OUTPUT_COMPARE);
+  Timer8.attachInterrupt(1, oneMSInterval);
+  Timer8.resume(); //Start Timer
+#else
+  Timer4.setPeriod(1000);  // Set up period
+  Timer4.setMode(1, TIMER_OUTPUT_COMPARE);
+  Timer4.attachInterrupt(1, oneMSInterval);
+  Timer4.resume(); //Start Timer
+#endif
+  pinMode(LED_BUILTIN, OUTPUT); //Visual WDT
+#endif
+
+  lastRPM_100ms = 0;
+  loop33ms = 0;
+  loop66ms = 0;
+  loop100ms = 0;
+  loop250ms = 0;
+  loopSec = 0;
+}
+
+
+//Timer2 Overflow Interrupt Vector, called when the timer overflows.
+//Executes every ~1ms.
+#if defined(CORE_AVR) //AVR chips use the ISR for this
+ISR(TIMER2_OVF_vect)
+#elif defined (CORE_TEENSY) || defined(CORE_STM32)
+void oneMSInterval() //Most ARM chips can simply call a function
+#endif
+{
+  ms_counter++;
+
+  //Increment Loop Counters
+  loop33ms++;
+  loop66ms++;
+  loop100ms++;
+  loop250ms++;
+  loopSec++;
+
+
+  //30Hz loop
+  if (loop33ms == 33)
+  {
+    loop33ms = 0;
+    BIT_SET(TIMER_mask, BIT_TIMER_30HZ);
+  }
+
+  //15Hz loop
+  if (loop66ms == 66)
+  {
+    loop66ms = 0;
+    BIT_SET(TIMER_mask, BIT_TIMER_15HZ);
+  }
+
+  //Loop executed every 100ms loop
+  //Anything inside this if statement will run every 100ms.
+  if (loop100ms == 100)
+  {
+    loop100ms = 0; //Reset counter
+    BIT_SET(TIMER_mask, BIT_TIMER_10HZ);
+
+   // currentStatus.rpmDOT = (currentStatus.RPM - lastRPM_100ms) * 10; //This is the RPM per second that the engine has accelerated/decelleratedin the last loop
+   // lastRPM_100ms = currentStatus.RPM; //Record the current RPM for next calc
+  }
+
+  //Loop executed every 250ms loop (1ms x 250 = 250ms)
+  //Anything inside this if statement will run every 250ms.
+  if (loop250ms == 250)
+  {
+    loop250ms = 0; //Reset Counter
+    BIT_SET(TIMER_mask, BIT_TIMER_4HZ);
+    #if defined(CORE_STM32) //debug purpose, only visal for running code
+      digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+    #endif
+
+    #if defined(CORE_AVR)
+      //Reset watchdog timer (Not active currently)
+      //wdt_reset();
+      //DIY watchdog
+      //This is a sign of a crash:
+      //if( (initialisationComplete == true) && (last250msLoopCount == mainLoopCount) ) { setup(); }
+      //else { last250msLoopCount = mainLoopCount; }
+    #endif
+  }
+
+  //Loop executed every 1 second (1ms x 1000 = 1000ms)
+  if (loopSec == 1000)
+  {
+    loopSec = 0; //Reset counter.
+    BIT_SET(TIMER_mask, BIT_TIMER_1HZ);
+
+   
+
+   
+     //NOTE - There is a potential for a ~1sec gap between engine crank starting and ths runSec number being incremented. This may delay ASE!
+      if (currentStatus.runSecs <= 254) //Ensure we cap out at 255 and don't overflow. (which would reset ASE)
+        { currentStatus.runSecs++; } //Increment our run counter by 1 second.
+    }
+    //**************************************************************************************************************************************************
+    //This records the number of main loops the system has completed in the last second
+    currentStatus.loopsPerSecond = mainLoopCount;
+    mainLoopCount = 0;
+    //**************************************************************************************************************************************************
+    //increament secl (secl is simply a counter that increments every second and is used to track whether the system has unexpectedly reset
+    currentStatus.secl++;
+    //**************************************************************************************************************************************************
+   
+  }
+#if defined(CORE_AVR) //AVR chips use the ISR for this
+    //Reset Timer2 to trigger in another ~1ms
+    TCNT2 = 131;            //Preload timer2 with 100 cycles, leaving 156 till overflow.
+    TIFR2  = 0x00;          //Timer2 INT Flag Reg: Clear Timer Overflow Flag
+#endif
+}
+
+#if defined(TIMER5_MICROS)
+//This is used by the fast version of micros(). We just need to increment the timer overflow counter
+ISR(TIMER5_OVF_vect)
+{
+  ++timer5_overflow_count;
+}
+
+static inline unsigned long micros_safe()
+{
+  unsigned long newMicros;
+  noInterrupts();
+  newMicros = micros();
+  interrupts();
+
+  return newMicros;
+}
+#endif
